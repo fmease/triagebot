105c622c0b958239187a7ba2fd40ef4590a80673
León Orell Valerian Liehr <me@fmease.dev>
2025-07-28 18:28:36 +0200

    [CLEANUP]

--- src/jobs.rs
+++ src/jobs.rs
@@ -7,31 +7,32 @@
 //! job (to be used as an identifier in the database) and the function to run
 //! when the job runs.
 //!
-//! The metadata is a serde_json::Value
-//! Please refer to https://docs.rs/serde_json/latest/serde_json/value/fn.from_value.html
-//! on how to interpret it as an instance of type T, implementing Serialize/Deserialize.
+//! The metadata is a [`serde_json::Value`].
+//! Please refer to [`serde_json::value::from_value`]. on how to interpret it
+//! as an instance of type `T`, implementing `Serialize` / `Deserialize`.
 //!
-//! The schedule is a cron::Schedule
-//! Please refer to https://docs.rs/cron/latest/cron/struct.Schedule.html for further info
+//! The schedule is a [`cron::Schedule`].
 //!
 //! ## Example, sending a zulip message once a week
 //!
 //! To give an example, let's imagine we want to sends a Zulip message every
-//! Friday at 11:30am ET into #t-release with a "@T-release meeting!"" content.
+//! Friday at 11:30am ET into #t-release with a "@T-release meeting!" content.
 //!
 //! To begin, let's create a generic zulip message Job:
-//!    #[derive(Serialize, Deserialize)]
-//!    struct ZulipMetadata {
-//!      pub message: String
-//!      pub channel: String,
-//!    }
-//!    struct ZulipMessageJob;
-//!    impl Job for ZulipMessageJob { ... }
+//!
+//!     #[derive(Serialize, Deserialize)]
+//!     struct ZulipMetadata {
+//!         pub message: String
+//!         pub channel: String,
+//!     }
+//!     struct ZulipMessageJob;
+//!     impl Job for ZulipMessageJob { ... }
 //!
 //! (Imagine that this job requires a channel and a message in the metadata.)
 //!
 //! If we wanted to have a default scheduled message, we could add the following to
 //! `default_jobs`:
+//!
 //!     JobSchedule {
 //!         name: ZulipMessageJob.name(),
 //!         schedule: Schedule::from_str("0 30 11 * * FRI *").unwrap(),
